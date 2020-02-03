e1a0a6b5e456702bddee7c24745fc7ba05d5fa38
Abdul Muttaleb Maruf <abdul.muttaleb@grameenphone.com>
2020-02-03 11:12:50 +0600

    Implemented filteredimage, updated util to handle connection timeout on image

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import Jimp = require('jimp');
-
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
@@ -9,19 +8,25 @@ import Jimp = require('jimp');
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+    return new Promise( async (resolve, reject) => {
+        try{
+            const photo = await Jimp.read(inputURL);
+            const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
+            await photo
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .write(__dirname+outpath, (img)=>{
+                    resolve(__dirname+outpath);
+                });
+        }catch (e) {
+            reject(e);
+        }
+
     });
 }
 
+// C:\Users\abdul.muttaleb\cfit\image-filter-starter-code\src\util\tmp\filtered.1460.jpg
 // deleteLocalFiles
 // helper function to delete files on the local disk
 // useful to cleanup after tasks
